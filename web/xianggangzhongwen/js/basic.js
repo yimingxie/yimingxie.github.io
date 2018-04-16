cdd52c78a2c6449b771724339ef5fa10c5abd208
yimingxie <329715712@qq.com>
2018-04-16 23:06:22 +0800

    香港中文大学

---
+++ web/xianggangzhongwen/js/basic.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Administrator on 2017/1/16.
+ */
+
+
+
+
+
+$(function () {
+
+    //加载公用部分，写程序时要删掉
+    $("header").load("header.html");
+    $("footer").load("footer.html");
+
+
+});
+
+
+
+
+
+
