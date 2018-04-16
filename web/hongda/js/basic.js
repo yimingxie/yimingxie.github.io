0685c2319205ef8a2d4e80af2a5c4b566c69d6a2
yimingxie <329715712@qq.com>
2018-04-16 22:28:46 +0800

    宏大检测

---
+++ web/hongda/js/basic.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2017/1/16.
+ */
+
+
+
+
+
+$(function () {
+
+    //加载公用部分，写程序时要删掉
+    $("header").load("header.html");
+    $(".vip-header").load("vip-header.html");
+    $(".vip-footer").load("vip-footer.html");
+    //$("footer").load("footer.html");
+
+
+});
+
+
+
+
+
+
